dec7507c3e371d4a0e593328d9fe3718b6a83a6a
Hyo Bin <a032875@gmail.com>
2024-03-13 07:26:51 +0900

    자료 업데이트

--- Enjoy-Coffee/src/api/DataFetcher.jsx
+++ Enjoy-Coffee/src/api/DataFetcher.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRecoilState } from "recoil";
 import { DataState } from "../recoil/recoilState";
+// import { CartState } from "../recoil/recoilState";
 import { fetchDataFromAPI } from "./api";
 
 const DataFetcher = () => {
@@ -19,3 +20,21 @@ const DataFetcher = () => {
 };
 
 export default DataFetcher;
+// const DataFetcher = () => {
+//   const [userData, setUserData] = useRecoilState(DataState);
+//   const [cart, setCart] = useRecoilState(CartState);
+
+//   React.useEffect(() => {
+//     const fetchData = async () => {
+//       const data = await fetchDataFromAPI();
+//       setUserData(data);
+//       setCart([]);
+//     };
+
+//     fetchData();
+//   }, [setUserData, setCart]);
+
+//   return null;
+// };
+
+// export default DataFetcher;
