b4f7371a71d250508ad9f30c7ca5460c6317c7a5
Hyo Bin <a032875@gmail.com>
2024-10-11 13:05:46 +0900

    mock데이터 만료로 인해 데이터 불러오는 방법 변경

--- Enjoy-Coffee/src/api/api.jsx
+++ Enjoy-Coffee/src/api/api.jsx
@@ -1,14 +1,4 @@
-// export const fetchDataFromAPI = async () => {
-//   const response = await fetch(
-//     "https://mocki.io/v1/b3d33504-ff01-4542-a35d-9f3c464ef978"
-//   );
-//   const data = await response.json();
-//   return data;
-// };
+import DB from "../../db.json";
 export const fetchDataFromAPI = async () => {
-  const response = await fetch(
-    "https://mocki.io/v1/532e877c-68e4-4bd5-b19f-90b47dc414e3"
-  );
-  const data = await response.json();
-  return data;
+  return DB;
 };
